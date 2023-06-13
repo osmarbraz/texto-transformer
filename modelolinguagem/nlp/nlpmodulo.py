3c97529dc0f75b800ed17c7bcf555a5d668750b2
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-06-13 20:02:10 -0300

    Update nlpmodulo.py

--- modelolinguagem/nlp/nlpmodulo.py
+++ modelolinguagem/nlp/nlpmodulo.py
@@ -40,92 +40,19 @@ class NLP():
         spacy_stopwords = self.nlp.Defaults.stop_words
 
         return spacy_stopwords 
-
-    # ============================
-    #def downloadSpacy(self):
-        #'''
-        #Realiza o download do arquivo do modelo para o diretório corrente.
-    
-        #'''
-        # Verifica se existe o diretório base
-        #DIRETORIO_MODELO_LINGUAGEM = verificaDiretorioModeloLinguagem()
-            
-        # Nome arquivo spacy
-        #ARQUIVO_MODELO_SPACY = self.model_args.modelo_spacy
-        # Versão spaCy
-        #VERSAO_SPACY = "-" + self.model_args.versao_spacy
-        # Nome arquivo compactado
-        #NOME_ARQUIVO_MODELO_COMPACTADO = ARQUIVO_MODELO_SPACY + VERSAO_SPACY + ".tar.gz"
-        
-        # Url do arquivo
-        #URL_ARQUIVO_MODELO_COMPACTADO = "https://github.com/explosion/spacy-models/releases/download/" + #ARQUIVO_MODELO_SPACY + VERSAO_SPACY + "/" + NOME_ARQUIVO_MODELO_COMPACTADO
-
-        # Realiza o download do arquivo do modelo
-        #logging.info("Download do arquivo do modelo do spaCy.")
-        #downloadArquivo(URL_ARQUIVO_MODELO_COMPACTADO, DIRETORIO_MODELO_LINGUAGEM + "/" + #NOME_ARQUIVO_MODELO_COMPACTADO)
-
-    # ============================   
-    #def descompactaSpacy(self):
-    #    '''
-    #    Descompacta o arquivo do modelo.
-    # 
-    #    '''
-        
-        # Verifica se existe o diretório base do cohebert e retorna o nome do diretório
-        #DIRETORIO_MODELO_LINGUAGEM = verificaDiretorioModeloLinguagem()
-        
-        # Nome arquivo spacy        
-        #ARQUIVO_MODELO_SPACY = self.model_args.modelo_spacy
-        # Versão spaCy
-        #VERSAO_SPACY = "-" + self.model_args.versao_spacy
-        
-        # Nome do arquivo a ser descompactado
-        #NOME_ARQUIVO_MODELO_COMPACTADO = DIRETORIO_MODELO_LINGUAGEM + "/" + ARQUIVO_MODELO_SPACY + VERSAO_SPACY + ".tar.gz"
-        
-        #logging.info("Descompactando o arquivo do modelo do spaCy.")
-        #arquivoTar = tarfile.open(NOME_ARQUIVO_MODELO_COMPACTADO, "r:gz")    
-        #arquivoTar.extractall(DIRETORIO_MODELO_LINGUAGEM)    
-        #arquivoTar.close()
-        
-        # Apaga o arquivo compactado
-        #if os.path.isfile(NOME_ARQUIVO_MODELO_COMPACTADO):
-        #    os.remove(NOME_ARQUIVO_MODELO_COMPACTADO)
-        
+       
     # ============================    
     def carrega(self):
         '''
         Realiza o carregamento da ferramenta de NLP.
-      
+     
         '''
-        
-        # Verifica se existe o diretório base
-        #DIRETORIO_MODELO_LINGUAGEM = verificaDiretorioModeloLinguagem()
-                      
-        # Nome arquivo spacy
-        #ARQUIVO_MODELO_SPACY = self.model_args.modelo_spacy
-        # Versão spaCy
-        #VERSAO_SPACY = "-" + self.model_args.versao_spacy
-        # Caminho raiz do modelo do spaCy
-        #DIRETORIO_MODELO_SPACY =  DIRETORIO_MODELO_LINGUAGEM + "/" + ARQUIVO_MODELO_SPACY + VERSAO_SPACY
-
-        # Verifica se o diretório existe
-        #if os.path.exists(DIRETORIO_MODELO_SPACY) == False:
-            # Realiza o download do arquivo modelo do spaCy
-        #    self.downloadSpacy()
-            # Descompacta o spaCy
-        #    self.descompactaSpacy()
-
-        # Diretório completo do spaCy
-        #DIRETORIO_MODELO_SPACY = DIRETORIO_MODELO_LINGUAGEM + "/" + ARQUIVO_MODELO_SPACY + VERSAO_SPACY + '/' + #ARQUIVO_MODELO_SPACY + '/' + ARQUIVO_MODELO_SPACY + VERSAO_SPACY + '/'
-
-        # Carrega o spaCy. Necessário somente 'tagger' para encontrar os substantivos
-        #self.nlp = spacy.load(DIRETORIO_MODELO_SPACY, disable=['tokenizer', 'lemmatizer', 'ner', 'parser', 'textcat', 'custom'])
-        
+          
         # Verifica se é necessário carregar a ferramenta
         if self.modelo_args.palavra_relevante != PalavrasRelevantes.ALL.value:
             # Carrega o modelo spacy
             print("Carregando o spaCy")
-            self.nlp = spacy.load(modelo_args.modelo_spacy,disable=['tokenizer', 'lemmatizer', 'ner', 'parser', 'textcat', 'custom'])    
+            self.nlp = spacy.load(self.modelo_args.modelo_spacy,disable=['tokenizer', 'lemmatizer', 'ner', 'parser', 'textcat', 'custom'])    
             print("spaCy carregado.")
         
         else:
