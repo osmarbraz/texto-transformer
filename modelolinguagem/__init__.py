5dafd966ebe1300af1fb31f6ea0a441caf20d652
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-06-15 18:54:46 -0300

    Update __init__.py

--- modelolinguagem/__init__.py
+++ modelolinguagem/__init__.py
@@ -1,6 +1,2 @@
 __version__ = "1.0.0"
 __MODELO_LINGUAGEM__ = 'modelo-linguagem'
-from .modelolinguagem import ModeloLinguagem
-from .modelo.transformer import Transformer
-from .nlp.nlpmodulo import NLP
-from .mensurador.mensurador import Mensurador
