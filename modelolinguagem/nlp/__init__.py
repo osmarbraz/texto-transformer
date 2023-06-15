6faa386754827c8aee0967142a6da50d0a1d4ef7
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-06-15 18:10:36 -0300

    Correção pacotes

--- modelolinguagem/nlp/__init__.py
+++ modelolinguagem/nlp/__init__.py
@@ -1,1 +1,1 @@
-from .nlpmodulo import NLP
+from nlpmodulo import NLP
--- modelolinguagem/util/utilarquivo.py
+++ modelolinguagem/util/utilarquivo.py
@@ -6,9 +6,9 @@ import os # Biblioteca para manipular arquivos
 import sys
 
 # Import de bibliotecas próprias
-from .utilmodulo import *
-from .utiltempo import *
-from .utilconstantes import *
+from util.utilmodulo import *
+from util.utiltempo import *
+from util.utilconstantes import *
 
 logger = logging.getLogger(__name__)
 
