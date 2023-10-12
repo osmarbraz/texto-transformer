9455be8b38f35c783c46c1da0a7a9442f1e40929
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-10-11 21:52:15 -0300

    Funções para criação de janela e testes

--- testes/util/test_utiltexto.py
+++ testes/util/test_utiltexto.py
@@ -6,7 +6,7 @@ import logging
 import unittest
 
 # Bibliotecas próprias
-from textotransformer.util.utiltexto import getIndexTokenTexto, getTextoLista, limpezaTexto, removeTags, tamanhoTexto
+from textotransformer.util.utiltexto import getIndexTokenTexto, getTextoLista, limpezaTexto, removeTags, tamanhoTexto, contaItensLista, getJanelaSentenca
 
 # Objeto de logger
 logger = logging.getLogger(__name__)
@@ -98,6 +98,129 @@ class TestUtilTexto(unittest.TestCase):
         self.assertEqual(idx_tokens[0], 7)
         self.assertEqual(idx_tokens[1], 13)
         self.assertEqual(idx_tokens[2], 18)
+        
+    # Testes contaItensLista
+    def test_contaItensLista(self):
+        logger.info("Testando o contaItensLista")
+        
+        # Valores de entrada
+        lista = [['P101', 'p102', 'p103', 'p104'], ['p105', 'p106', 'p107', 'p108']]
+                              
+        # Avalia a saida do método       
+        self.assertEqual(contaItensLista(lista), 8)
+
+    # Testes getJanelaSentenca3DocumentoMaior
+    def test_getJanelaSentenca3DocumentoMaior(self):
+        logger.info("Testando o getJanelaSentenca3 com lista de documento maior que a janela ")
+        
+        # Valores de entrada
+        tamanho_janela = 3
+        trunca_janela = 30
+
+        # Lista de sentenças
+        lista_sentenca = ['P101 p102 p103 p104 p105 p106 p107 p108 p109 p110 p111 p112 p113 p114 p115 p116 p117 p118 p119 p120 .',
+                        'p201 p202 p203 p204 p205 p206 p207 p208 p209 p210 p211 p212 p213 p214 p215 p216 p217 p218 p219 p220 p221 p222 p223 p224 .',
+                        'p301 p302 p303 p304 p305 p306 p307 p308 p309 p310 p311 p312 p313 p314 p315 p316 p317 p318 p319 p320 p321 p322 p323 p324 p325.',
+                        'p401 p402 p403 p404 p405 p406 p407 p408 p409 p410 p411 p412 p413 p414 p415 p416 p417 p418 p419 p420 p421 p422 p423 p424 p425 p426.',
+                        'p501 p502 p503 p504 p505 p506 p507 p508 p509 p510 p511 p512 p513 p514 p515 p516 p517 p518 p519 p520 p521 p522 p523 p524 p525.',
+                        'p601 p602 p603 p604 p605 p606 p607 p608 p609 p610 p611 p612 p613 p614 p615 p616 p617 p618 p619 p620 p621 p622 p623 p624 .',
+                        'p701 p702 p703 p704 p705 p706 p707 p708 p709 p710 p711 p712 p713 p714 p715 p716 p717 p718 p719 p720 p721 p722 p723 .']
+
+        # Divide a sentença em palavras pelos espaços.
+        lista = []
+        for i, sentenca in enumerate(lista_sentenca):
+            # Divide a sentença pelos espaços em branco.
+            lista.append([sentenca.split(' ')])
+        
+        # Lista das janelas para comparar os elementos.
+        lista_saida_janela = []
+        lista_saida_centro = []
+        lista_saida_indice = []
+        for i, x in enumerate(lista):
+            lista_janela, string_janela, lista_indice_janela, centro_janela = getJanelaSentenca(lista, tamanho_janela, i, trunca_janela)
+            lista_saida_janela.append(lista_janela)
+            lista_saida_centro.append(centro_janela)
+            lista_saida_indice.append(lista_indice_janela)
+        
+        lista_esperada_janela = [[['P101', 'p102', 'p103', 'p104', 'p105', 'p106', 'p107', 'p108', 'p109', 'p110', 'p111', 'p112', 'p113', 'p114', 'p115', 'p116', 'p117', 'p118', 'p119', 'p120', '.'], ['p201', 'p202', 'p203', 'p204', 'p205', 'p206', 'p207', 'p208', 'p209']], [[], ['p201', 'p202', 'p203', 'p204', 'p205', 'p206', 'p207', 'p208', 'p209', 'p210', 'p211', 'p212', 'p213', 'p214', 'p215', 'p216', 'p217', 'p218', 'p219', 'p220', 'p221', 'p222', 'p223', 'p224', '.'], ['p301', 'p302', 'p303', 'p304']], [['p224', '.'], ['p301', 'p302', 'p303', 'p304', 'p305', 'p306', 'p307', 'p308', 'p309', 'p310', 'p311', 'p312', 'p313', 'p314', 'p315', 'p316', 'p317', 'p318', 'p319', 'p320', 'p321', 'p322', 'p323', 'p324', 'p325.'], ['p401', 'p402', 'p403']], [['p324', 'p325.'], ['p401', 'p402', 'p403', 'p404', 'p405', 'p406', 'p407', 'p408', 'p409', 'p410', 'p411', 'p412', 'p413', 'p414', 'p415', 'p416', 'p417', 'p418', 'p419', 'p420', 'p421', 'p422', 'p423', 'p424', 'p425', 'p426.'], ['p501', 'p502']], [['p424', 'p425', 'p426.'], ['p501', 'p502', 'p503', 'p504', 'p505', 'p506', 'p507', 'p508', 'p509', 'p510', 'p511', 'p512', 'p513', 'p514', 'p515', 'p516', 'p517', 'p518', 'p519', 'p520', 'p521', 'p522', 'p523', 'p524', 'p525.'], ['p601', 'p602']], [['p523', 'p524', 'p525.'], ['p601', 'p602', 'p603', 'p604', 'p605', 'p606', 'p607', 'p608', 'p609', 'p610', 'p611', 'p612', 'p613', 'p614', 'p615', 'p616', 'p617', 'p618', 'p619', 'p620', 'p621', 'p622', 'p623', 'p624', '.'], ['p701', 'p702']], [['p620', 'p621', 'p622', 'p623', 'p624', '.'], ['p701', 'p702', 'p703', 'p704', 'p705', 'p706', 'p707', 'p708', 'p709', 'p710', 'p711', 'p712', 'p713', 'p714', 'p715', 'p716', 'p717', 'p718', 'p719', 'p720', 'p721', 'p722', 'p723', '.']]] 
+        lista_esperada_centro = [[0], [1], [1], [1], [1], [1], [1]]
+        lista_esperada_indice = [[0, 1], [0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6]]
+
+        self.assertListEqual(lista_saida_janela, lista_esperada_janela)
+        self.assertListEqual(lista_saida_centro, lista_esperada_centro)
+        self.assertListEqual(lista_saida_indice, lista_esperada_indice)
+        
+    # Testes getJanelaSentenca3DocumentoIgualJanela
+    def test_getJanelaSentenca3DocumentoIgualJanela(self):
+        logger.info("Testando o getJanelaSentenca3 com lista de documento igual a janela ")
+        
+        # Valores de entrada
+        tamanho_janela = 3
+        trunca_janela = 30
+
+        # Lista de sentenças
+        lista_sentenca = ['P101 p102 p103 p104 p105 p106 p107 p108 p109 p110 .', 'p201 p202 p203 p204 p205 p206 p207 p208 p209 p210.', 'p301 p302 p303 p304 p305 p306 p307 p308 p309 p310']
+
+        # Divide a sentença em palavras pelos espaços.
+        lista = []
+        for i, sentenca in enumerate(lista_sentenca):
+            # Divide a sentença pelos espaços em branco.
+            lista.append([sentenca.split(' ')])
+        
+        # Lista das janelas para comparar os elementos.
+        lista_saida_janela = []
+        lista_saida_centro = []
+        lista_saida_indice = []
+        #for i, x in enumerate(lista.iterrows():
+        for i, x in enumerate(lista):
+            lista_janela, string_janela, lista_indice_janela, centro_janela = getJanelaSentenca(lista, tamanho_janela, i, trunca_janela)
+            lista_saida_janela.append(lista_janela)
+            lista_saida_centro.append(centro_janela)
+            lista_saida_indice.append(lista_indice_janela)
+        
+        lista_esperada_janela = [[['P101', 'p102', 'p103', 'p104', 'p105', 'p106', 'p107', 'p108', 'p109', 'p110', '.'], ['p201', 'p202', 'p203', 'p204', 'p205', 'p206', 'p207', 'p208', 'p209', 'p210.'], ['p301', 'p302', 'p303', 'p304', 'p305', 'p306', 'p307', 'p308', 'p309', 'p310']], [['P101', 'p102', 'p103', 'p104', 'p105', 'p106', 'p107', 'p108', 'p109', 'p110', '.'], ['p201', 'p202', 'p203', 'p204', 'p205', 'p206', 'p207', 'p208', 'p209', 'p210.'], ['p301', 'p302', 'p303', 'p304', 'p305', 'p306', 'p307', 'p308', 'p309', 'p310']], [['P101', 'p102', 'p103', 'p104', 'p105', 'p106', 'p107', 'p108', 'p109', 'p110', '.'], ['p201', 'p202', 'p203', 'p204', 'p205', 'p206', 'p207', 'p208', 'p209', 'p210.'], ['p301', 'p302', 'p303', 'p304', 'p305', 'p306', 'p307', 'p308', 'p309', 'p310']]]
+        lista_esperada_centro = [[1], [1], [1]]
+        lista_esperada_indice = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+
+        self.assertListEqual(lista_saida_janela, lista_esperada_janela)
+        self.assertListEqual(lista_saida_centro, lista_esperada_centro)
+        self.assertListEqual(lista_saida_indice, lista_esperada_indice)   
+        
+    # Testes getJanelaSentenca3DocumentoMenorJanela
+    def test_getJanelaSentenca3DocumentoMenorJanela(self):
+        logger.info("Testando o getJanelaSentenca3 com lista de documento menor que janela ")
+        
+        # Valores de entrada
+        tamanho_janela = 2
+        trunca_janela = 30
+
+        # Lista de sentenças
+        lista_sentenca = ['P101 p102 p103 p104 p105 p106 p107 p108 p109 p110 .']
+
+        # Divide a sentença em palavras pelos espaços.
+        lista = []
+        for i, sentenca in enumerate(lista_sentenca):
+            # Divide a sentença pelos espaços em branco.
+            lista.append([sentenca.split(' ')])
+        
+        # Lista das janelas para comparar os elementos.
+        lista_saida_janela = []
+        lista_saida_centro = []
+        lista_saida_indice = []
+        #for i, x in enumerate(lista.iterrows():
+        for i, x in enumerate(lista):
+            lista_janela, string_janela, lista_indice_janela, centro_janela = getJanelaSentenca(lista, tamanho_janela, i, trunca_janela)
+            lista_saida_janela.append(lista_janela)
+            lista_saida_centro.append(centro_janela)
+            lista_saida_indice.append(lista_indice_janela)
+        
+        lista_esperada_janela = [[['P101', 'p102', 'p103', 'p104', 'p105', 'p106', 'p107', 'p108', 'p109', 'p110', '.']]]
+        lista_esperada_centro = [[0]]
+        lista_esperada_indice = [[0]]
+
+        self.assertListEqual(lista_saida_janela, lista_esperada_janela)
+        self.assertListEqual(lista_saida_centro, lista_esperada_centro)
+        self.assertListEqual(lista_saida_indice, lista_esperada_indice)
         
 if "__main__" == __name__:
     logger = logging.getLogger()
--- textotransformer/util/utiltexto.py
+++ textotransformer/util/utiltexto.py
@@ -247,3 +247,324 @@ def contaElemento(lista, elemento):
         cont = cont + 1
         
     return cont
+
+# ============================
+def contaItensLista(lista):
+  """
+     Conta os itens das lista de listas.
+
+     Parâmetros:
+       `lista` - Uma lista de lista itens.
+
+     Retorno:
+       `qtde_itens` - Quantidade de itens das listas.
+  """
+  # Quantidade itens da lista
+  qtde_itens = 0
+
+  for sentenca in lista:
+    qtde_itens = qtde_itens + len(sentenca)
+
+  return qtde_itens
+
+# ============================
+def truncaJanela(lista, lista_janela, tamanho_janela, lista_indice_janela, indice_passo, maximo_itens, folha_janela):
+    """
+     Trunca as palavras da janela até o máximo da janela.
+
+     Parâmetros:
+       Parâmetros:
+       `lista` - Uma lista com todos os itens.
+       `lista_janela` - Um dataframe com os itens.
+       `tamanho_janela` - Tamanho da janela a ser montada.
+       `lista_indice_janela` - Lista com os índices das sentenças que forma a janela.
+       `indice_passo` - Índice do passo que se deseja da janela.
+       `maximo_itens` - Máximo de palavras na janela. Trunca das extremidades preservando a palavra central.
+
+     Retorno:
+       `lista_janela` - Janela truncada pelo máximo de itens.
+    """
+    # Quantidade de itens nas janelas antes
+    qtde_itens1 = contaItensLista(lista_janela)
+    # print("quantidade de itens janela antes:", qtde_itens1)
+
+    # Controle se não alcançado o máximo de palavras
+    minimo_alcancado = False
+
+    # Remove as palavras das extremidade que ultrapassam o tamanho máximo
+    while qtde_itens1 > maximo_itens and minimo_alcancado == False:
+
+      # Verifica se é uma janela de início
+      if indice_passo < folha_janela and len(lista_janela) != tamanho_janela:
+        # print("Janelas do inicio")
+        # Verifica se a última sentença tem elementos
+        if len(lista_janela[-1]) >0:
+          # Meio - Remove do fim da janela da última sentença
+          del lista_janela[-1][-1]
+        else:
+          # Verifica se a penúltima sentença tem elementos
+          if len(lista_janela[-2]) >0:
+            # Meio - Remove do fim da janela da penúltima sentença
+            del lista_janela[-2][-1]
+          else:
+            # Verifica se a antepenúltima sentença tem elementos
+            if len(lista_janela[-3]) >0:
+              # Meio - Remove do fim da janela da antepenúltima sentença
+              del lista_janela[-3][-1]
+      else:
+        # Verifica se é uma janela do meio
+        if indice_passo < len(lista)-folha_janela and len(lista_janela) == tamanho_janela:
+          # print("Janelas do meio")
+          # Verifica se a primeira sentença tem elementos
+          if len(lista_janela[0]) >0:
+            # Meio - Remove do inicio da janela da 1a sentença
+            del lista_janela[0][0]
+          else:
+            # Verifica se a segunda sentença tem elementos
+            if len(lista_janela[1]) >0:
+              # Meio - Remove do inicio da janela da 2a sentença
+              del lista_janela[1][0]
+
+          # Verifica se a última sentença tem elementos
+          if len(lista_janela[-1]) >0:
+            # Meio - Remove do fim da janela da 1a sentença
+            del lista_janela[-1][-1]
+          else:
+            # Verifica se a penúltima sentença tem elementos
+            if len(lista_janela[-2]) >0:
+              # Meio - Remove do fim da janela da 2a sentença
+              del lista_janela[-2][-1]
+        else:
+            # Verifica se é uma janela de fim
+            if indice_passo >= len(lista)-folha_janela and len(lista_janela) != tamanho_janela:
+              # Verifica se a primeira sentença tem elementos
+              if len(lista_janela[0]) >0:
+                # Fim - Remove do início da janela da 1a sentença
+                del lista_janela[0][0]
+              else:
+                # Verifica se a segunda sentença tem elementos
+                if len(lista_janela[1]) >0:
+                  # Fim - Remove do início da janela da 2a sentença
+                  del lista_janela[1][0]
+                else:
+                  # Verifica se a terceira sentença tem elementos
+                  if len(lista_janela[2]) >0:
+                    # Fim - Remove do início da janela da 3a sentença
+                    del lista_janela[2][0]
+
+      # Calcula a nova quantidade de itens
+      qtde_itens2 = contaItensLista(lista_janela)
+      # print("quantidade de itens janela durante:", qtde_itens2)
+
+      # Verifica se conseguiu reduzir a quantidade de itens
+      if (qtde_itens1 == qtde_itens2):
+        print("Atenção: Truncamento de janela não conseguiu reduzir além de ", qtde_itens2, " para o máximo ", maximo_itens)
+        minimo_alcancado = True
+
+      # Atribui uma nova quantidade
+      qtde_itens1 = qtde_itens2
+
+    return lista_janela
+
+# ============================
+def getJanelaSentenca(lista, tamanho_janela, indice_passo, maximo_itens=None):
+  """
+     Cria janelas de itens de uma lista
+
+     Parâmetros:
+       `lista` - Uma lista com todos os itens.
+       `tamanho_janela` - Tamanho da janela a ser montada.
+       `indice_passo` - Índice do passo que se deseja da janela.
+       `maximo_itens` - Máximo de itens na janela. Trunca das extremidades preservando a palavra central.
+
+     Retorno:
+       `lista_janela` - Lista com os itens em janelas.
+       `string_janela` - String com os itens em janelas.
+       `lista_indice_janela` - Lista com os índices das sentenças que forma a janela.
+       `lista_centro_janela` - Lista com os índices dos centros da janela.
+  """
+
+  # Se a lista é menor que o tamanho da janela  
+  if len(lista) <= tamanho_janela:
+
+    # Recupera a sentenças da janela
+    lista_janela = []
+    for i, sentenca in enumerate(lista):
+        lista_janela.append(sentenca[0])
+
+    # Guarda os índices dos itens das janelas
+    lista_indice_janela = []
+
+    # Adiciona os índices das janelas
+    for i in range(len(lista)):
+      lista_indice_janela.append(i)
+
+    # Guarda os índices dos centros das janelas
+    lista_centro_janela = []
+    # Calcula o centro
+    centro_janela = int((len(lista)/2))
+    lista_centro_janela.append(centro_janela)
+
+    # Concatena em uma string as palavras das sentenças da janela
+    lista_janela_sentenca = []
+    for sentenca in lista_janela:
+      lista_janela_sentenca.append(" ".join(sentenca))
+          
+    return lista_janela, " ".join(lista_janela_sentenca), lista_indice_janela, lista_centro_janela
+
+  else:
+    # print(">>>> Lista maior que as janelas")
+    # Lista maior que o tamanho da janela
+    # Calcula o tamanho da folha da janela(quantidade de valores a esquerda e direitado centro da janela).
+    folha_janela = int((tamanho_janela-1) /2)
+    # print("folha_janela:", folha_janela)
+    # print("indice_passo:", indice_passo)
+    # Define o centro da janela
+    centro_janela = -1
+    # Percorre a lista
+    # Dentro do intervalo da lista de itens
+    if indice_passo >= 0 and indice_passo < len(lista):
+
+      # Seleciona o passo que se deseja a janela de
+      # Guarda os itens da janela
+      lista_janela = []
+      # Guarda os índices dos itens das janelas
+      lista_indice_janela = []
+      # Guarda os índices dos centros das janelas
+      lista_centro_janela = []
+
+      # Inicio da lista sem janelas completas antes do passo
+      if indice_passo < folha_janela:
+        # print("Inicio da lista")
+        # Sentenças anteriores
+        #Evita estourar o início da lista
+        inicio = 0
+        fim = indice_passo
+        # print("Anterior: inicio:", inicio, " fim:", fim)
+        for j in range(inicio, fim):
+          # Recupera o documento da lista
+          documento = lista[j]
+          lista_janela.append(documento[0])
+          # Adiciona o indice do documento na lista
+          lista_indice_janela.append(j)
+
+        # Sentença central
+        # Recupera o documento da lista
+        documento = lista[indice_passo]
+        lista_janela.append(documento[0])
+        # Adiciona o indice do documento na lista
+        lista_indice_janela.append(indice_passo)
+        # Guarda o centro da janela
+        centro_janela = len(lista_janela)-1
+        lista_centro_janela.append(centro_janela)
+
+        # Sentenças posteriores
+        inicio = indice_passo + 1
+        fim = indice_passo + folha_janela + 1
+        # print("Posterior: inicio:", inicio, " fim:", fim)
+        for j in range(inicio,fim):
+          # Recupera o documento da lista
+          documento = lista[j]
+          lista_janela.append(documento[0])
+          # Adiciona o indice do documento na lista
+          lista_indice_janela.append(j)
+
+      else:
+        # Meio da lista com janelas completas antes e depois
+        if indice_passo < len(lista)-folha_janela:
+          # print(" Meio da lista")
+
+          # Sentenças anteriores
+          inicio = indice_passo - folha_janela
+          fim = indice_passo
+          # print("inicio:", inicio, " fim:", fim)
+          for j in range(inicio, fim):
+            # Recupera o documento da lista
+            documento = lista[j]
+            # Adiciona o documento a janela
+            lista_janela.append(documento[0])
+            # Adiciona o indice do documento na lista
+            lista_indice_janela.append(j)
+
+          # Sentença central
+          # Recupera o documento da lista
+          documento = lista[indice_passo]
+          # Adiciona o documento a janela
+          lista_janela.append(documento[0])          
+          # Adiciona o indice do documento na lista
+          lista_indice_janela.append(indice_passo)
+          # Guarda o centro da janela
+          centro_janela = len(lista_janela)-1
+          lista_centro_janela.append(centro_janela)
+
+          # Sentenças posteriores
+          inicio = indice_passo + 1
+          fim = indice_passo + 1 + folha_janela
+          for j in range(inicio,fim):
+            # Recupera o documento da lista
+            documento = lista[j]
+            # Adiciona o documento a janela
+            lista_janela.append(documento[0])
+            # Adiciona o indice do documento na lista
+            lista_indice_janela.append(j)
+
+        else:
+          # Fim da lista sem janelas completas depois
+          if indice_passo >= len(lista)-folha_janela:
+            # print("Fim da lista")
+
+            # Sentenças anteriores
+            inicio = indice_passo - folha_janela
+            fim = indice_passo
+            #print("inicio:", inicio, " fim:", fim)
+            for j in range(inicio, fim):
+              # Recupera o documento da lista
+              documento = lista[j]
+              # Adiciona o documento a janela
+              lista_janela.append(documento[0])
+              # Adiciona o indice do documento na lista
+              lista_indice_janela.append(j)
+
+            # Sentença central
+            # Recupera o documento da lista
+            documento = lista[indice_passo]
+            # Adiciona o documento a janela
+            lista_janela.append(documento[0])
+            # Adiciona o indice do documento na lista
+            lista_indice_janela.append(indice_passo)
+            # Guarda o centro da janela
+            centro_janela = len(lista_janela)-1
+            lista_centro_janela.append(centro_janela)
+
+            # Sentenças posteriores
+            inicio = indice_passo + 1
+            fim = indice_passo + 1 + folha_janela
+            # Evita o extrapolar o limite da lista de sentenças
+            if fim > len(lista):
+              fim = len(lista)
+            for j in range(inicio,fim):
+              # Recupera o documento da lista
+              documento = lista[j]
+              # Adiciona o documento a janela
+              lista_janela.append(documento[0])
+              # Adiciona o indice do documento na lista
+              lista_indice_janela.append(j)
+    else:
+      print("Índice fora do intervalo da lista de itens.")
+
+    # Se existir maximo_itens realiza o truncamento
+    if maximo_itens != None:
+      # Cria uma copia da lista de itens para evitar a referência
+      lista_apagar = []
+      for item in lista_janela:
+          lista_apagar.append(item.copy())
+
+      # Trunca a quantidade de itens da janela até o máximo de itens.
+      lista_janela = truncaJanela(lista, lista_apagar, tamanho_janela, lista_indice_janela, indice_passo, maximo_itens, folha_janela)
+      
+    # Junta em uma string as palavras das sentenças da janela
+    lista_janela_sentenca = []
+    for sentenca in lista_janela:
+      lista_janela_sentenca.append(" ".join(sentenca))
+
+    return lista_janela, " ".join(lista_janela_sentenca), lista_indice_janela, lista_centro_janela
